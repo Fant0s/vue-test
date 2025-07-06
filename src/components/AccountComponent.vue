bfc213df78e2741f802119c4f20669709f7bbd3d
Alex <110283025+Fant0s@users.noreply.github.com>
2025-07-06 21:08:12 +0500

    add validation + save

--- src/components/AccountComponent.vue
+++ src/components/AccountComponent.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import InputComponent from '@/components/InputComponent.vue'
 import { ref } from 'vue'
-import { type IAccount } from '@/types/account.ts'
+import { AccountType, type IAccount } from '@/types/account.ts'
 import SelectComponent from '@/components/SelectComponent.vue'
+import { useAccountStore } from '@/stores/account.ts'
 
 interface IProps {
   account: IAccount
@@ -14,22 +15,62 @@ const emit = defineEmits<{
   (e: 'delete', id: number): void
 }>()
 
-const error = ref(false)
+const errors = ref<{ [key: string]: boolean }>({})
+const store = useAccountStore()
 
 const handleDelete = () => {
   emit('delete', props.account.id)
 }
+
+const handleSave = (field: 'login' | 'marks' | 'password', value: string | null) => {
+  const updated = { ...props.account, [field]: value }
+  store.updateAccount(updated)
+  validation(field)
+}
+
+const validation = (field: 'login' | 'marks' | 'password') => {
+  const value = props.account[field]
+
+  if (field === 'login' && (!value || value.length > 100)) {
+    errors.value.login = true
+  } else if (field === 'marks' && typeof value === 'string' && value.length > 50) {
+    errors.value.marks = true
+  } else if (
+    field === 'password' &&
+    props.account.type === AccountType.Local &&
+    (!value || value.length > 100)
+  ) {
+    errors.value.password = true
+  } else {
+    errors.value[field] = false
+  }
+}
 </script>
 
 <template>
-  <div class="account">
-    <InputComponent type="marks" :value="props.account.marks" :error="error" />
+  <div class="account" :class="{ LDAP: props.account.type === AccountType.LDAP }">
+    <InputComponent
+      type="marks"
+      :value="props.account.marks"
+      :error="errors.marks"
+      @update:value="(val) => handleSave('marks', val)"
+    />
 
-    <SelectComponent :error="error" />
+    <SelectComponent :error="errors.type" @update:value="(val) => handleSave('type', val)" />
 
-    <InputComponent type="login" :value="props.account.login" :error="error" />
+    <InputComponent
+      type="login"
+      :value="props.account.login"
+      :error="errors.login"
+      @update:value="(val) => handleSave('login', val)"
+    />
 
-    <InputComponent type="password" :value="props.account.password" :error="error" />
+    <InputComponent
+      type="password"
+      :value="props.account.password"
+      :error="errors.password"
+      @update:value="(val) => handleSave('password', val)"
+    />
 
     <button class="delete" @click="handleDelete">delete</button>
   </div>
@@ -38,6 +79,8 @@ const handleDelete = () => {
 <style lang="sass" scoped>
 .account
   display: grid
-  grid-template-columns: 1fr 1fr 1fr 1fr 50px
+  grid-template-columns: 1fr .7fr 1fr 1fr 50px
   gap: 10px
+  &.LDAP
+    grid-template-columns: 1fr .7fr 2fr 50px
 </style>
