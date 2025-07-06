b1af48062d70f63e1e452491ceb25f8b0fbfc946
Alex <110283025+Fant0s@users.noreply.github.com>
2025-07-06 12:30:23 +0500

    add form

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="form">
+    <div class="top">
+      <h1>Учетные записи</h1>
+      <button class="add-account">+</button>
+    </div>
+    <p class="hint">
+      <strong>Подсказка:</strong> В поле <em>"Метка"</em> вводите текстовые метки через точку с
+      запятой `;`. Максимум 50 символов.
+    </p>
+    <div class="accounts-list"></div>
+  </div>
+</template>
+
+<style scoped></style>
