7809e5764144992186ca577a13719c584bd6ba04
Alex <110283025+Fant0s@users.noreply.github.com>
2025-07-06 13:46:30 +0500

    add store

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,16 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAccountStore } from '@/stores/account.ts'
+import AccountComponent from '@/components/AccountComponent.vue'
+
+const store = useAccountStore()
+
+const handleAddAccount = () => {
+  store.addAccount()
+}
+</script>
 
 <template>
   <div class="form">
     <div class="top">
       <h1>Учетные записи</h1>
-      <button class="add-account">+</button>
+      <button class="add-account" @click="handleAddAccount">+</button>
     </div>
     <p class="hint">
       <strong>Подсказка:</strong> В поле <em>"Метка"</em> вводите текстовые метки через точку с
       запятой `;`. Максимум 50 символов.
     </p>
-    <div class="accounts-list"></div>
+    <div class="accounts-list">
+      <AccountComponent v-for="account in store.list" :key="account.id" />
+    </div>
   </div>
 </template>
 
