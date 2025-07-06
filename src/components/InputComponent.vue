883cdccdb2da44800be66abe583251351c638937
Alex <110283025+Fant0s@users.noreply.github.com>
2025-07-06 15:56:25 +0500

    add styles

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import type { TMark } from '@/types/account.ts'
 
 interface IProps {
+  value: string | TMark[] | null
   type: 'login' | 'password' | 'marks'
   error?: boolean
 }
 
 const props = withDefaults(defineProps<IProps>(), {
+  value: '',
   error: false,
 })
 
@@ -18,7 +21,9 @@ const toggleVisibility = () => {
   inputType.value = inputType.value === 'password' ? 'text' : 'password'
 }
 
-const loseFocus = () => {}
+const handleLoseFocus = () => {
+  isFocused.value = false
+}
 </script>
 
 <template>
@@ -31,7 +36,7 @@ const loseFocus = () => {}
       placeholder="Значение"
       :type="props.type === 'password' ? inputType : type"
       @focus="isFocused = true"
-      @blur="loseFocus"
+      @blur="handleLoseFocus"
     />
     <template v-if="props.type === 'password'">
       <button class="visible" @click="toggleVisibility">
@@ -41,4 +46,24 @@ const loseFocus = () => {}
   </div>
 </template>
 
-<style scoped></style>
+<style lang="sass" scoped>
+.input
+  display: flex
+  justify-content: center
+  align-items: center
+  border: 2px solid gray
+  transition: .3s ease-out
+  border-radius: 5px
+  input
+    width: 100%
+    padding: 10px
+    border: unset
+    outline: none
+    background-color: transparent
+  &.error
+    border-color: red
+    color: red
+    background-color: #ffc8c8
+  &.focus
+    border-color: black
+</style>
