e948328c66361349e85e233a81458e1d21b72b4f
Alex <110283025+Fant0s@users.noreply.github.com>
2025-07-07 14:48:40 +0500

    add styles

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -2,6 +2,7 @@
 import { useAccountStore } from '@/stores/account.ts'
 import AccountComponent from '@/components/AccountComponent.vue'
 import { useModalsStore } from '@/stores/modals.ts'
+import HintIcon from '@/components/icons/hintIcon.vue'
 
 const store = useAccountStore()
 const modalStore = useModalsStore()
@@ -18,12 +19,15 @@ const handleRemoveAccount = (id: number) => {
   <div class="form">
     <div class="top">
       <h1 class="page-title">Учетные записи</h1>
-      <button class="add-account" @click="handleAddAccount">+</button>
+      <button class="add-account" @click="handleAddAccount" aria-label="add account">+</button>
     </div>
-    <p class="hint">
-      <strong>Подсказка:</strong> В поле <em>"Метка"</em> вводите текстовые метки через точку с
-      запятой `;`. Максимум 50 символов.
-    </p>
+    <div class="hint">
+      <hint-icon />
+      <p>
+        В поле <em>"Метка"</em> вводите текстовые метки через точку с запятой `;`. Максимум 50
+        символов.
+      </p>
+    </div>
     <table class="accounts-list">
       <template v-if="store.list.length">
         <thead>
@@ -54,9 +58,37 @@ const handleRemoveAccount = (id: number) => {
 <style lang="sass" scoped>
 .form
   .top
-    .page-title
+    display: flex
+    flex-direction: row
+    align-items: center
+    gap: 20px
     .add-account
+      display: flex
+      justify-content: center
+      align-items: center
+      width: 40px
+      height: 40px
+      font-size: 24px
+      border-radius: 10px
+      border-color: #c0c0ff
+      color: #0000ff
+      background-color: transparent
+      transition: .3s ease-out
+      &:hover
+        background-color: #c0c0ff
   .hint
+    display: flex
+    flex-direction: row
+    gap: 10px
+    align-items: center
+    margin: 30px 0 20px
+    padding: 15px
+    border-radius: 10px
+    background-color: rgba(203, 210, 255, 0.28)
+    font-weight: 500
+    svg
+      width: 40px
+      height: 40px
   .accounts-list
     border-collapse: collapse
     width: 100%
@@ -64,7 +96,8 @@ const handleRemoveAccount = (id: number) => {
       .cell
         .cell-content
           display: flex
-          padding: 0 5px
+          padding: 0 5px 10px
+          color: #989898
     .not-found
       display: flex
       justify-content: center
